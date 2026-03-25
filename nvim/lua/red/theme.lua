420586bd0f9c917b5cb9ab240c0c5d57b5402c9b
redsonlopez <redsonlopez@gmail.com>
2026-03-25 08:35:55 -0300

    create color variables

--- nvim/lua/red/theme.lua
+++ nvim/lua/red/theme.lua
@@ -3,40 +3,50 @@ local api  = vim.api
 -- Colors Cheme
 vim.cmd.colorscheme("monokai-pro-spectrum")
 
+-- Theme Colors
+local base_bg         = "#222222"
+local base_bg_light   = "#2e2e2e"
+local base_bg_popup   = "#333333"
+local fg_muted        = "#888888"
+local fg_dim          = "#545454"
+local fg_separator    = "#777777"
+local accent          = "#ffaa00"
+
+
 api.nvim_create_autocmd({ "ColorScheme", "FileType" }, {
   pattern = { "*", "NvimTree" },
   callback = function()
     -- Tabline - Tittle
-    api.nvim_set_hl(0, "TabLineFill", { bg = "#222222" })
-    api.nvim_set_hl(0, "TabLineSel", { fg = "#222222", bg = "#ffaa00" })
-    api.nvim_set_hl(0, "TabLine", { fg = "#888888", bg = "#222222" })
+    api.nvim_set_hl(0, "TabLineFill", { bg = base_bg })
+    api.nvim_set_hl(0, "TabLineSel", { fg = base_bg, bg = accent })
+    api.nvim_set_hl(0, "TabLine", { fg = fg_muted, bg = base_bg })
     -- Nvim Tree
-    api.nvim_set_hl(0, "NvimTreeNormal",      { bg = "#222222" })
-    api.nvim_set_hl(0, "NvimTreeNormalNC",    { bg = "#222222" })
-    api.nvim_set_hl(0, "NvimTreeWinSeparator",{ bg = "#222222", fg = "#777777" })
-    api.nvim_set_hl(0, "NvimTreeEndOfBuffer",  { bg = "#222222", fg = "#777777" })
-    api.nvim_set_hl(0, "NvimTreeVertSplit",    { bg = "#222222", fg = "#777777" })
-    api.nvim_set_hl(0, "NvimTreeCursorLine",   { bg = "#2e2e2e" })
-    api.nvim_set_hl(0, "NvimTreeSignColumn",   { bg = "#222222" })
+    api.nvim_set_hl(0, "NvimTreeNormal",      { bg = base_bg })
+    api.nvim_set_hl(0, "NvimTreeNormalNC",    { bg = base_bg })
+    api.nvim_set_hl(0, "NvimTreeWinSeparator",{ bg = base_bg, fg = fg_separator })
+    api.nvim_set_hl(0, "NvimTreeEndOfBuffer",  { bg = base_bg, fg = fg_separator })
+    api.nvim_set_hl(0, "NvimTreeVertSplit",    { bg = base_bg, fg = fg_separator })
+    api.nvim_set_hl(0, "NvimTreeCursorLine",   { bg = base_bg_light })
+    api.nvim_set_hl(0, "NvimTreeSignColumn",   { bg = base_bg })
     -- Number + Fold
-    api.nvim_set_hl(0, "LineNr",          { fg = "#545454" })
-    api.nvim_set_hl(0, "FoldColumn",      { bg = "#222222" })
-    api.nvim_set_hl(0, "CursorLine",      { bg = "#2e2e2e" })
-    api.nvim_set_hl(0, "CursorLineNr",    { fg = "#888888" })
-    api.nvim_set_hl(0, "CursorLineFold",  { bg = "#222222" })
+    api.nvim_set_hl(0, "LineNr",          { fg = fg_dim })
+    api.nvim_set_hl(0, "FoldColumn",      { bg = base_bg })
+    api.nvim_set_hl(0, "CursorLine",      { bg = base_bg_light })
+    api.nvim_set_hl(0, "CursorLineNr",    { fg = fg_muted })
+    api.nvim_set_hl(0, "CursorLineFold",  { bg = base_bg })
     -- BackGround
-    api.nvim_set_hl(0, "Normal",      { bg = "#222222" })
-    api.nvim_set_hl(0, "NormalFloat", { bg = "#222222" })
-    api.nvim_set_hl(0, "FloatBorder", { bg = "#222222" })
-    api.nvim_set_hl(0, "Pmenu",       { bg = "#333333" })
+    api.nvim_set_hl(0, "Normal",      { bg = base_bg })
+    api.nvim_set_hl(0, "NormalFloat", { bg = base_bg })
+    api.nvim_set_hl(0, "FloatBorder", { bg = base_bg })
+    api.nvim_set_hl(0, "Pmenu",       { bg = base_bg_popup })
     -- BackGround Foot
-    api.nvim_set_hl(0, "EndOfBuffer", { bg = "#222222", fg = "#222222" })
-    api.nvim_set_hl(0, "NonText",     { bg = "#222222", fg = "#222222" })
-    api.nvim_set_hl(0, "Whitespace",  { bg = "#222222", fg = "#222222" })
+    api.nvim_set_hl(0, "EndOfBuffer", { bg = base_bg, fg = base_bg })
+    api.nvim_set_hl(0, "NonText",     { bg = base_bg, fg = base_bg })
+    api.nvim_set_hl(0, "Whitespace",  { bg = base_bg, fg = base_bg })
     -- Statusline
     api.nvim_set_hl(0, "StatusLine", {
-      bg = "#222222",
-      fg = "#888888",
+      bg = base_bg,
+      fg = fg_muted,
       bold = true,
       italic = true,
     })
